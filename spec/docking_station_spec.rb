9c7131513a7efd210cf2c436f6854383977baafa
Prabu Sathan <prabusathan@Prabus-MacBook-Pro.local>
2017-05-09 14:27:29 +0100

    rspec update/creating release_bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,7 @@
-describe DockingStation do
+require "docking_station"
 
+describe DockingStation do
+  it "assigns object in the DockingStation to the bike variable" do
+    expect(DockingStation.new).to respond_to(:release_bike)
+  end
 end
