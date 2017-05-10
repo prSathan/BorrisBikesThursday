9fbf0539e8365abb660de7fa60b116b5ca95490c
Prabu Sathan <prabusathan@Prabus-MacBook-Pro.local>
2017-05-10 17:56:34 +0100

    Step 13/14

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -19,7 +19,7 @@ describe DockingStation do
   it "returns docked bikes" do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bike).to eq bikes
   end
 
  describe '#release_bike' do
@@ -30,7 +30,7 @@ describe DockingStation do
 
   describe '#dock' do
   it "Raises an error when full" do
-    subject.dock(Bike.new)
+    20.times { subject.dock(Bike.new) }
     expect { subject.dock(Bike.new) }.to raise_error("Docking station full")
   end
  end
