38a731587e42d0c0c5a7db6910bb80123c5513b7
Spencer <spencer.bartonfisher@me.com>
2017-05-11 12:26:42 +0100

    Single Responsibility Principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,21 +2,30 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_reader :bike
+
   def initialize
     @bikes = []
   end
 
   def release_bike
-     fail "No bikes available" if @bikes.empty?
+      fail "No bikes available" if empty?
      @bikes.pop
   end
 
   def dock(bike)
-    fail "Docking station full" if @bikes.count >= 20
+    fail "Docking station full" if full?
     @bikes << bike
   end
 
-  attr_reader :bike
+  private
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
+  end
 
 end
 
